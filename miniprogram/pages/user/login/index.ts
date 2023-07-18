16fbcc72d4fdb08d55a3ba2f24cdfaa3b015cd3a
cheng_xq <1060943419@qq.com>
2023-07-18 22:16:48 +0800

    add: 添加首页

--- miniprogram/pages/user/login/index.ts
+++ miniprogram/pages/user/login/index.ts
@@ -4,26 +4,36 @@ Page({
   data:{
 
   },
+  pageTo(){
+    console.log(22)
+    wx.navigateTo({
+            url:'../../home/home'
+          }) 
+  },
   getPhoneNumber (e:any) {
+    console.log(11)
     let detail  = e.detail;
     console.log(detail)
-    if(detail.errMsg === "getPhoneNumber:ok") {
-      console.log('用户同意授权')
-      let code  = detail.code;
-      console.log(code)
-      wx.request({
-        //登录接口
-        url:'',
-        data:{
-          code
-        },
-        success(res:any) {
-          console.log(res.data)
-        }
-      })
-    }else {
-      console.log('用户拒绝授权')
-    }
+    // if(detail.errMsg === "getPhoneNumber:ok") {
+    //   console.log('用户同意授权')
+    //   let code  = detail.code;
+    //   console.log(code)
+    //   wx.request({
+    //     //登录接口
+    //     url:'',
+    //     data:{
+    //       code
+    //     },
+    //     success(res:any) {
+    //       // wx.navigateTo({
+    //       //   url:'../../home/home'
+    //       // }) 
+    //       console.log(res.data)
+    //     }
+    //   })
+    // }else {
+    //   console.log('用户拒绝授权')
+    // }
 
   }
 
