dbb8e390e73e69547e2d792871b41ad69617e54a
liqiang <liqiang@blockindex.com>
2023-07-18 23:15:33 +0800

    fix:解决登录页冲突

--- miniprogram/pages/user/login/index.ts
+++ miniprogram/pages/user/login/index.ts
@@ -14,26 +14,29 @@ Page({
     console.log(11)
     let detail = e.detail;
     console.log(detail)
-    // if(detail.errMsg === "getPhoneNumber:ok") {
-    //   console.log('用户同意授权')
-    //   let code  = detail.code;
-    //   console.log(code)
-    //   wx.request({
-    //     //登录接口
-    //     url:'',
-    //     data:{
-    //       code
-    //     },
-    //     success(res:any) {
-    //       // wx.navigateTo({
-    //       //   url:'../../home/home'
-    //       // }) 
-    //       console.log(res.data)
-    //     }
-    //   })
-    // }else {
-    //   console.log('用户拒绝授权')
-    // }
+    wx.navigateTo({
+      url:'../../home/home'
+    }) 
+    if(detail.errMsg === "getPhoneNumber:ok") {
+      console.log('用户同意授权')
+      let code  = detail.code;
+      console.log(code)
+      // wx.request({
+      //   //登录接口
+      //   url:'',
+      //   data:{
+      //     code
+      //   },
+      //   success(res:any) {
+      //     // wx.navigateTo({
+      //     //   url:'../../home/home'
+      //     // }) 
+      //     console.log(res.data)
+      //   }
+      // })
+    }else {
+      console.log('用户拒绝授权')
+    }
 
   }
 
